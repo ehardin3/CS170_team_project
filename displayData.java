4ac0ea6b006025c2afd4685167db3f225aeb3da2
TOPFanboy <pwnagefuliciouser@gmail.com>
2025-05-04 22:50:11 -0700

    Update displayData.java

--- displayData.java
+++ displayData.java
@@ -3,7 +3,7 @@ import java.util.Map;
 public class displayData {
 	public static void showGrades(Map<String, Map<String, Double>> data) {
 		for(String sem : data.keySet()) {
-			System.out.println("In the " + sem + " semester, " + data.get(sem).get("A") + "% students got A's, while " + data.get(sem).get("D") + "%% got D's.\n");
+			System.out.println("In the " + sem + " semester, " + data.get(sem).get("A") + "% students got A's, while " + data.get(sem).get("D") + "% got D's.");
 		}
 	}
 }
