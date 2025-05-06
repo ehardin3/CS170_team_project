2c0e0452697641e65a3dd8d72c808cccf8182675
TOPFanboy <pwnagefuliciouser@gmail.com>
2025-05-05 23:25:31 -0700

    Update displayData.java

--- displayData.java
+++ displayData.java
@@ -1,7 +1,18 @@
-import java.util.Map;
+import java.util.*;
 
 public class displayData {
 	public static void showGrades(Map<String, Map<String, Double>> data) {
+		Set<String> temp = data.keySet();
+		Iterator<String> it1 = temp.iterator();
+		Iterator<String> it2 = temp.iterator();
+		String first = it1.next();
+		while(it1.hasNext()) {
+			it1.next();
+			it2.next();
+		}
+		String last = it2.next();
+		System.out.println("OHLONE STATS FOR SEMESTERS FROM " + first.toUpperCase() + " TO " + last.toUpperCase());
+		System.out.println("-------------------------------------------------------------------------\n");
 		for(String sem : data.keySet()) {
 			System.out.println("In the " + sem + " semester, " + data.get(sem).get("A") + "% students got A's, while " + data.get(sem).get("D") + "% got D's.");
 		}
