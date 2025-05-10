74d25f847ea299050bd79c3c25a1e3664ce11efe
TOPFanboy <pwnagefuliciouser@gmail.com>
2025-05-09 21:44:59 -0700

    Update CS170_team_project.java

--- CS170_team_project.java
+++ CS170_team_project.java
@@ -2,7 +2,7 @@ package CS170_team_project;
 
 import java.util.*;
 
-public class groupProject {
+public class CS170_team_project {
     public static void main(String[] args) {
         String filePath = "C:\\Users\\pwnag\\Downloads\\GradesSumm.csv";
         Map<String, Map<String, Double>> gradeData = readFromFile.getGrades(filePath);
