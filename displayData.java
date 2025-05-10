ecdaebd3416b22e894f5024055fa6985c719c7b4
TOPFanboy <pwnagefuliciouser@gmail.com>
2025-05-10 01:21:37 -0700

    Update displayData.java

--- displayData.java
+++ displayData.java
@@ -1,20 +1,13 @@
 import java.util.*;
 
 public class displayData {
-	public static void showGrades(Map<String, Map<String, Double>> data) {
-		Set<String> temp = data.keySet();
-		Iterator<String> it1 = temp.iterator();
-		Iterator<String> it2 = temp.iterator();
-		String first = it1.next();
-		while(it1.hasNext()) {
-			it1.next();
-			it2.next();
-		}
-		String last = it2.next();
-		System.out.println("OHLONE STATS FOR SEMESTERS FROM " + first.toUpperCase() + " TO " + last.toUpperCase());
+	public static void showGrades(Map<Integer, Map<String, Map<String, Double>>> data) {
+		System.out.println("OHLONE STATS FOR SEMESTERS FROM " + data.get(0).keySet().toString() + " TO " + data.get(data.keySet().size() - 1).keySet().toString());
 		System.out.println("-------------------------------------------------------------------------\n");
-		for(String sem : data.keySet()) {
-			System.out.println("In the " + sem + " semester, " + data.get(sem).get("A") + "% students got A's, while " + data.get(sem).get("D") + "% got D's.");
+		for(int i = 0; i <= data.keySet().size() - 1; i++) {
+			Map<String, Map<String, Double>> sems = data.get(i);
+			for(String sem : sems.keySet())
+				System.out.println("In the " + sem + " semester, " + sems.get(sem).get("A") + "% students got A's, while " + sems.get(sem).get("D") + "% got D's.");
 		}
 	}
 }
