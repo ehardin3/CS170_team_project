93dab2070d681b76c48738590c0fcb6ee525fcc0
TOPFanboy <pwnagefuliciouser@gmail.com>
2025-05-09 21:26:07 -0700

    Update CS170_team_project.java

--- CS170_team_project.java
+++ CS170_team_project.java
@@ -2,39 +2,10 @@ package CS170_team_project;
 
 import java.util.*;
 
-public class CS170_team_project {
+public class groupProject {
     public static void main(String[] args) {
-        String filePath = "C:/Users/keybl/Downloads/GradesSumm.csv";
-
-        // Get the grades from the file
-        Map<String, List<Double>> gradeData = readFromFile.getGrades(filePath);
-
-        // Show some Grade A values
-        System.out.println("Grade A Sample:");
-        List<Double> gradeA = gradeData.get("A");
-        if (gradeA != null) {
-            int count = 0;
-            for (int i = 0; i < gradeA.size(); i++) {
-                System.out.println(gradeA.get(i) + "%");
-                count++;
-                if (count == 2) {
-                    break;
-                }
-            }
-        }
-
-        // Show some Grade B values
-        System.out.println("\nGrade B Sample:");
-        List<Double> gradeB = gradeData.get("B");
-        if (gradeB != null) {
-            int count = 0;
-            for (int i = 0; i < gradeB.size(); i++) {
-                System.out.println(gradeB.get(i) + "%");
-                count++;
-                if (count == 2) {
-                    break;
-                }
-            }
-        }
+        String filePath = "C:\\Users\\pwnag\\Downloads\\GradesSumm.csv";
+        Map<String, Map<String, Double>> gradeData = readFromFile.getGrades(filePath);
+        displayData.showGrades(gradeData);
     }
 }
