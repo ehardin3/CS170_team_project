00662bb424df3545c5561792d0f04199bcba4162
ehardin3 <ehardin3@student.ohlone.edu>
2025-05-09 18:45:19 -0700

    Update readFromFile.java

--- readFromFile.java
+++ readFromFile.java
@@ -14,15 +14,24 @@ public class readFromFile {
             String line; // Stores each line
             List<String[]> data = new ArrayList<>(); // List to hold each row split into columns
             String delimiter = ","; // Set delimiter to a comma so we know where to spilt the data
-
+           
             // Reads the file line by line
             while ((line = br.readLine()) != null) {
                 // Each line is split into columns using the delimiter and added to the data list
                 data.add(line.split(delimiter));
             }
-
+            
+            if (data.size() > 4) {
+                // Row 0 assumed to contain the semester names
+                List<String> semesters = getStrings(data.get(0));
+                // Row 3 (index 3) contains grade A percentages
+                List<Double> gradeAList = getNumbers(data.get(3));
+                // Row 4 (index 4) contains grade B percentages
+                List<Double> gradeBList = getNumbers(data.get(4));
+            } 
+                        
             // Checks if there are at least 4 rows and gets data from the 4th row (index 3)
-            if (data.size() > 3) {
+          /*  if (data.size() > 3) {
                 // Get the values from the 4th row and store them under key "A" using the helper function
                 grades.put("A", getNumbers(data.get(3)));
             }
@@ -31,7 +40,7 @@ public class readFromFile {
             if (data.size() > 4) {
                 // Get the values from the 5th row and store them under key "B" using the helper function
                 grades.put("B", getNumbers(data.get(4)));
-            }
+            } */
 
         } catch (IOException e) {
             // If there's an error while reading the file, print a message
@@ -39,10 +48,20 @@ public class readFromFile {
         }
 
         // Return the map that contains the grades
-        return grades;
+        return null; 
+        
     }
 
-    private static List<Double> getNumbers(String[] row) {
+    private static List<String> getStrings(String[] row) {
+        List<String> strings = new ArrayList<>();
+        // Start at column 3(skip the commas) and go though grabbing the strings
+        for (int i = 2; i < row.length; i++) {
+            strings.add(row[i].trim());
+        }
+        return strings;
+    }
+
+	private static List<Double> getNumbers(String[] row) {
         List<Double> numbers = new ArrayList<>(); // List to store the numbers
 
         // Start from the 5th column (index 4) and go through the rest of the row
