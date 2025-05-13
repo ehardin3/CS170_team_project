cfb866f8158a9638bf215a3f459d07c9a420fa7e
TOPFanboy <pwnagefuliciouser@gmail.com>
2025-05-13 01:21:52 -0700

    Update readFromFile.java

--- readFromFile.java
+++ readFromFile.java
@@ -4,10 +4,10 @@ import java.io.*;
 import java.util.*;
 
 public class readFromFile {
-
+	
+//	  public static Map<Integer, Map<String, Map<String, Double>>> getGrades (String filePath)
     public static Map<String, Map<String, Double>> getGrades(String filePath) {
-        // Map to store grades for A and D as lists of numbers
-        Map<String, List<Double>> grades = new HashMap<>();
+        // Map to store semesters and stats for A and D grades
         List<String> semesters = new ArrayList<>();
         List<Double> gradeAList = new ArrayList<>();
         List<Double> gradeDList = new ArrayList<>();
@@ -24,7 +24,7 @@ public class readFromFile {
                 data.add(line.split(delimiter));
             }
             
-            if (data.size() > 6) { // checks for minimum  list size
+            if (data.size() > 6) {
                 // Row 0 contains the semester names
                 semesters = getStrings(data.get(0));
                 // Row 3 contains grade A percentages
@@ -32,23 +32,12 @@ public class readFromFile {
                 // Row 4 contains grade D percentages
                 gradeDList = getNumbers(data.get(6));
             } 
-            
-            // Checks if there are at least 4 rows and gets data from the 4th row (index 3)
-            if (data.size() > 3) {
-                // Get the values from the 4th row and store them under key "A" using the helper function
-                grades.put("A", getNumbers(data.get(3)));
-            }
-
-            // Checks if there are at least 5 rows and gets data from the 5th row (index 4)
-            if (data.size() > 4) {
-                // Get the values from the 5th row and store them under key "B" using the helper function
-                grades.put("D", getNumbers(data.get(4)));
-            } 
 
         } catch (IOException e) {
             // If there's an error while reading the file, print a message
             System.out.println("Error reading file.");
         }
+        
         //Put everything into a map
         //Make the outer map
         Map<String, Map<String, Double>> data = new TreeMap<>();
@@ -69,7 +58,40 @@ public class readFromFile {
         return data; 
         
     }
-
+/*        
+        //Make a list of ordered semesters for sorting map later
+        List<String> orderedSems = new ArrayList<>();
+        orderedSems = orderSemestersChronologically(semesters);
+        
+        //Put everything into a map
+        //Make the outer map
+        Map<Integer, Map<String, Map<String, Double>>> sortedSemData = new TreeMap<>();
+        //For each element in the semesters array
+        for (int i = 0; i < semesters.size(); i++)
+        {
+            //make the innermost map HashMap<String, Double> innerMap
+        	HashMap<String, Double> innerMap = new HashMap<>();
+            //innerMap.put("A", gradeA[i])
+        	innerMap.put("A", gradeAList.get(i));
+            //innerMap.put("D", gradeD[i++])
+        	innerMap.put("D", gradeDList.get(i));
+            //Make the middle map and data.put(element, innermap)
+        	Map<String, Map<String, Double>> semData = new HashMap<>();
+        	semData.put(semesters.get(i), innerMap);
+        	//Assign index of current semester in ordered list as location in TreeMap
+        	sortedSemData.put(orderedSems.indexOf(semesters.get(i)), semData);
+        }
+        
+        //Order the map using its position from orderedSems
+        
+        for(int i = 0; i < semesters.size(); i++) {
+        	sortedSemData.put(i, semData);
+        }
+ 
+        // Return the map that contains the grades
+        return sortedSemData;         
+    }
+*/
     private static List<String> getStrings(String[] row) {
         List<String> strings = new ArrayList<>();
         // Start at column 3(skip the commas) and go though grabbing the strings
@@ -107,4 +129,45 @@ public class readFromFile {
         // Return the list of numbers (percentages)
         return numbers;
     }
+/*
+    // Helper method which defines semesters (Spring is 1, Summer is 2, Fall is 3)
+    private static int termOrder(String term) {
+        switch (term) {
+            case "Spring": return 1;
+            case "Summer": return 2;
+            case "Fall": return 3;
+            default: return 0;
+        }
+    }
+    // Method that takes list of unsorted semesters and sorts them into chronological order
+    public static List<String> orderSemestersChronologically(List<String> sem)
+    {
+            sem.sort((s1, s2) -> {
+            String[] parts1 = s1.split(" ");
+            String[] parts2 = s2.split(" ");
+
+            if (parts1.length < 2 || parts2.length < 2) return 0;
+
+            String term1 = parts1[0];
+            String term2 = parts2[0];
+
+            int year1 = Integer.parseInt(parts1[1]);
+            int year2 = Integer.parseInt(parts2[1]);
+
+            // First compares by year
+            int yearCompare = Integer.compare(year1, year2);
+            if (yearCompare != 0) return yearCompare;
+
+            // If years are the same, compares by term order
+            return Integer.compare(termOrder(term1), termOrder(term2));
+        });
+
+        System.out.println("Semesters displayed in chronological order:");
+        for (String s:sem)
+        {
+        System.out.println(" * " + s);
+    }
+        return sem;
+*/
+    
 }
